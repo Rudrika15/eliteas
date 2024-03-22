584098577f33e1c26ce77203c6e1ccb75a5e4ff8
Rudrika15 <rudrikadave20226@gmail.com>
2024-03-22 11:40:46 +0530

    logout functionility done

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,26 +10,24 @@
     <meta content="" name="keywords">
 
     <!-- Favicons -->
-    <link href="{{asset('img/favicon.png')}}" rel="icon" />
-    <link href="{{asset('img/apple-touch-icon.png')}}" rel="apple-touch-icon" />
+    <link href="{{ asset('img/favicon.png') }}" rel="icon" />
+    <link href="{{ asset('img/apple-touch-icon.png') }}" rel="apple-touch-icon" />
 
     <!-- Google Fonts -->
     <link href="https://fonts.gstatic.com" rel="preconnect">
-    <link
-        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
-    <link href="{{asset('vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet" />
-    <link href="{{asset('vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet" />
-    <link href="{{asset('vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet" />
-    <link href="{{asset('vendor/quill/quill.snow.css')}}" rel="stylesheet" />
-    <link href="{{asset('vendor/remixicon/remixicon.css')}}" rel="stylesheet" />
-    <link href="{{asset('vendor/simple-datatables/style.css')}}" rel="stylesheet" />
+    <link href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet" />
+    <link href="{{ asset('vendor/boxicons/css/boxicons.min.css') }}" rel="stylesheet" />
+    <link href="{{ asset('vendor/quill/quill.snow.css') }}" rel="stylesheet" />
+    <link href="{{ asset('vendor/remixicon/remixicon.css') }}" rel="stylesheet" />
+    <link href="{{ asset('vendor/simple-datatables/style.css') }}" rel="stylesheet" />
 
 
     <!-- Template Main CSS File -->
-    <link href="{{asset('css/style.css')}}" rel="stylesheet" />
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" />
 
     <!-- =======================================================
   * Template Name: NiceAdmin
@@ -45,8 +43,7 @@
     <main>
         <div class="container">
 
-            <section
-                class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
+            <section class="section register min-vh-100 d-flex flex-column align-items-center justify-content-center py-4">
                 <div class="container">
                     <div class="row justify-content-center">
                         <div class="col-lg-4 col-md-6 d-flex flex-column align-items-center justify-content-center">
@@ -58,42 +55,36 @@
                                 </a>
                             </div><!-- End Logo -->
 
-                            <div class="card mb-3">
+                            <div class="card mb-3" style="width: 20rem;">
                                 <div class="card-body">
                                     <h5 class="card-title text-center pb-0 fs-4">Login</h5>
                                     {{-- <p class="text-center small">Enter your email & password to login</p> --}}
 
-                                    <form method="POST" action="{{ route('login') }}" class="needs-validation"
-                                        novalidate>
+                                    <form method="POST" action="{{ route('login') }}" class="needs-validation w-100" novalidate>
                                         @csrf
 
                                         <div class="mb-3 form-floating">
-                                            <input id="email" type="email"
-                                                class="form-control @error('email') is-invalid @enderror" name="email"
-                                                value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                            <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                                             <label for="email">Email Address</label>
                                             @error('email')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
                                             @enderror
                                         </div>
 
                                         <div class="mb-3 form-floating">
-                                            <input id="password" type="password"
-                                                class="form-control @error('password') is-invalid @enderror"
-                                                name="password" required autocomplete="current-password">
+                                            <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                                             <label for="password">Password</label>
                                             @error('password')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
                                             @enderror
                                         </div>
 
                                         <div class="mb-3 form-check">
-                                            <input type="checkbox" class="form-check-input" name="remember"
-                                                id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                            <input type="checkbox" class="form-check-input" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
                                             <label class="form-check-label" for="remember">Remember Me</label>
                                         </div>
 
@@ -102,9 +93,9 @@
                                         </div>
 
                                         @if (Route::has('password.request'))
-                                        <div class="mt-3 text-center">
-                                            <a href="#">Forgot Your Password?</a>
-                                        </div>
+                                            <div class="mt-3 text-center">
+                                                <a href="#">Forgot Your Password?</a>
+                                            </div>
                                         @endif
                                     </form>
                                 </div>
@@ -129,23 +120,22 @@
         </div>
     </main><!-- End #main -->
 
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-            class="bi bi-arrow-up-short"></i></a>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
 
-    <script src="{{asset('vendor/apexcharts/apexcharts.min.js')}}"></script>
-    <script src="{{asset('vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-    <script src="{{asset('vendor/chart.js/chart.umd.js')}}"></script>
-    <script src="{{asset('vendor/echarts/echarts.min.js')}}"></script>
-    <script src="{{asset('vendor/quill/quill.min.js')}}"></script>
-    <script src="{{asset('vendor/simple-datatables/simple-datatables.js')}}"></script>
-    <script src="{{asset('vendor/tinymce/tinymce.min.js')}}"></script>
-    <script src="{{asset('vendor/php-email-form/validate.js')}}"></script>
+    <script src="{{ asset('vendor/apexcharts/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('vendor/chart.js/chart.umd.js') }}"></script>
+    <script src="{{ asset('vendor/echarts/echarts.min.js') }}"></script>
+    <script src="{{ asset('vendor/quill/quill.min.js') }}"></script>
+    <script src="{{ asset('vendor/simple-datatables/simple-datatables.js') }}"></script>
+    <script src="{{ asset('vendor/tinymce/tinymce.min.js') }}"></script>
+    <script src="{{ asset('vendor/php-email-form/validate.js') }}"></script>
 
 
     <!-- Template Main JS File -->
-    <script src="{{asset('js/main.js')}}"></script>
+    <script src="{{ asset('js/main.js') }}"></script>
 
 </body>
 
