9458567879d333710d7555c1f60e1306f9060aa4
Parvez Memon <137087093+Parvez2003@users.noreply.github.com>
2024-04-24 11:22:04 +0530

    design

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -340,8 +340,8 @@
         </div>
     </footer><!-- End Footer -->
 
-    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
-            class="bi bi-arrow-up-short"></i></a>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center "
+        style="background-color: #1d2865; "><i class="bi bi-arrow-up-short"></i></a>
 
     <!-- Vendor JS Files -->
 
